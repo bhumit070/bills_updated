b46a687c912bedf3f150d83c76dd1f4b2fa85ff9
bhumit070 <ganatrabhoomit070@gmail.com>
2021-11-20 21:13:04 +0530

    added firm api

--- api/index.js
+++ api/index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+app.use(express.json())
+
 // routes
+const firmRoutes = require('./routes/firm')
 
+app.get('/', (req, res) => res.status(200).json({ ok: "ok" }))
+app.use(firmRoutes)
 
+app.use((req, res) => {
+	return res.status(404).json({
+		error: true,
+		message: 'Route not found'
+	})
+})
 
 // db connection
 db.sequelize.sync().then(() => {
