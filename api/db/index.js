7c33d674aaca957f768674d1ebc8608e3943fd0d
bhumit070 <ganatrabhoomit070@gmail.com>
2021-11-24 00:46:48 +0530

    add host to database connection

--- api/db/index.js
+++ api/db/index.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
 	process.env.DB_USERNAME,
 	process.env.DB_PASSWORD,
 	{
+		host: process.env.DB_HOST || 'localhost',
 		dialect: process.env.DB_DIALECT || 'mysql',
 		pool: {
 			max: 5,
