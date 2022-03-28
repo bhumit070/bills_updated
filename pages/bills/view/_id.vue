591910a82a5810750658e5f3b96ca0398a39b890
bhumit070 <ganatrabhoomit070@gmail.com>
2022-03-28 23:02:44 +0530

    added delete bill feature

--- pages/bills/view/_id.vue
+++ pages/bills/view/_id.vue
@@ -8,55 +8,59 @@
       class="mt-2 mb-2 w-25 ml-5 pl-5 hide-print"
       @firm-changed="handleFirmChange"
     />
-    <b-button variant="primary" class="mt-2 mb-2 ml-5 hide-print">
-      Print Bill
-    </b-button>
+    <b-button variant="primary" class="mt-2 mb-2 ml-5 hide-print">Print Bill</b-button>
     <div v-if="!loading && !bills.length && !selectedFirm" class="text-center mt-5 pt-5">
-      <h3>
-          Select firm to view Bills.
-      </h3>
+      <h3>Select firm to view Bills.</h3>
     </div>
     <div v-if="!loading && !bills.length && selectedFirm" class="text-center mt-5 pt-5">
-      <h3>
-          No bills found
-      </h3>
+      <h3>No bills found</h3>
     </div>
     <div v-if="bills.length && !loading" class="bill-card">
       <b-card>
         <b-card-header>
           <div class="float-right pb-3">
             DATE:
-            <span v-if="!dateEdit" @click="dateEdit = true">
-              {{ getDate() }}
-            </span>
+            <span v-if="!dateEdit" @click="dateEdit = true">{{ getDate() }}</span>
             <div v-if="dateEdit">
-              <b-input v-model="date" type="date"> </b-input>
-              <b-button @click="dateEdit = false"> Select Date </b-button>
+              <b-input v-model="date" type="date"></b-input>
+              <b-button @click="dateEdit = false">Select Date</b-button>
             </div>
           </div>
           <table v-if="firm">
             <tr>
-              <td><b>FIRM</b></td>
+              <td>
+                <b>FIRM</b>
+              </td>
               <td>{{ firm.name }}</td>
             </tr>
             <tr>
-              <td><b>ADDRESS</b></td>
+              <td>
+                <b>ADDRESS</b>
+              </td>
               <td>{{ firm.address }}</td>
             </tr>
             <tr>
-              <td><b>PAN</b></td>
+              <td>
+                <b>PAN</b>
+              </td>
               <td>{{ firm.pan }}</td>
             </tr>
             <tr>
-              <td><b>Bank Account Name</b></td>
+              <td>
+                <b>Bank Account Name</b>
+              </td>
               <td>{{ firm.bank_name }}</td>
             </tr>
             <tr>
-              <td><b>Bank Account Number</b></td>
+              <td>
+                <b>Bank Account Number</b>
+              </td>
               <td>{{ firm.bank_account_number }}</td>
             </tr>
             <tr>
-              <td><b>Bank Account IFSC Code</b></td>
+              <td>
+                <b>Bank Account IFSC Code</b>
+              </td>
               <td>{{ firm.bank_ifsc_code }}</td>
             </tr>
           </table>
@@ -71,20 +75,23 @@
         </b-card-header>
         <b-card-body v-if="bills && bills.length">
           <b-table :items="bills" :fields="fields">
-            <template #cell(name)="item">
-              {{ item.item.seller.name }}
-            </template>
-            <template #cell(date)="item">
-              {{ getDate(item.item.date) }}
-            </template>
-            <template #cell(commodity)="item">
-              {{ item.item.grain.name }}
+            <template #cell(name)="item">{{ item.item.seller.name }}</template>
+            <template #cell(date)="item">{{ getDate(item.item.date) }}</template>
+            <template #cell(commodity)="item">{{ item.item.grain.name }}</template>
+            <template #cell(action)="item">
+              <b-button
+                variant="danger"
+                :disabled="deleteBillLoading"
+                @click.stop="deleteBill(item.item.id)"
+              >
+                <b-icon icon="trash-fill" aria-hidden="true"></b-icon>
+              </b-button>
             </template>
           </b-table>
         </b-card-body>
         <b-card-footer>
           <div class="text-center" style="font-size: 20px">
-            <b> Total: {{ totalAmount }} </b>
+            <b>Total: {{ totalAmount }}</b>
           </div>
         </b-card-footer>
       </b-card>
@@ -104,6 +111,7 @@ export default {
     selectedFirm: null,
     firm: null,
     dateEdit: false,
+    deleteBillLoading: false,
     date: `${new Date().getFullYear()}-03-31`,
     fields: [
       'date',
@@ -114,6 +122,7 @@ export default {
       'bags',
       'dalali_rate',
       'amount',
+      'action'
     ],
   }),
   computed: {
@@ -141,9 +150,23 @@ export default {
   },
 
   methods: {
+    async deleteBill(id) {
+      this.deleteBillLoading = true
+      try {
+        const deleteConfirmation = confirm("Are you sure you want to delete this bill ?")
+        if (deleteConfirmation) {
+          await axios.delete(`/api/bill/${id}`)
+          await this.getBills()
+        }
+      } catch (error) {
+        alert("Failed to delete bill.")
+      } finally {
+        this.deleteBillLoading = false
+      }
+    },
     async getBills() {
       try {
-        if(!this.personId || !this.selectedFirm) return
+        if (!this.personId || !this.selectedFirm) return
         this.bills = []
         this.loading = true
         const { data } = await axios.get(
